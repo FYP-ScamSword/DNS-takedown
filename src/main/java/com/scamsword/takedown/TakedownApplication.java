f3b9f45f99a6f08d19cfa00e6b54af0851b1192e
Emmanuel Oh <emmaneugene@outlook.com>
2023-01-18 07:32:50 +0800

    feat: Initialize Spring project

---
+++ src/main/java/com/scamsword/takedown/TakedownApplication.java
@@ -0,0 +1,21 @@
+package com.scamsword.takedown;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class TakedownApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TakedownApplication.class, args);
+	}
+
+	@GetMapping("/health")
+	public String health() {
+		return "Service is healthy!";
+	} 
+}
---
+++ src/test/java/com/scamsword/takedown/TakedownApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scamsword.takedown;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TakedownApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
