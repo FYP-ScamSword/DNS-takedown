8462231ab728f8016efcdb6291e32f4f97e74117
Emmanuel Oh <emmaneugene@outlook.com>
2023-01-30 12:57:08 +0800

    feat: Change /health output to JSON, remove whitelabel error page

--- src/main/java/com/scamsword/takedown/TakedownApplication.java
+++ src/main/java/com/scamsword/takedown/TakedownApplication.java
@@ -2,9 +2,19 @@ package com.scamsword.takedown;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.Calendar;
 
@@ -14,14 +24,34 @@ import org.openqa.selenium.WebDriver;
 @RestController
 public class TakedownApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TakedownApplication.class, args);
-	}
+    public static void main(String[] args) {
+        ApplicationContext ctx = SpringApplication.run(TakedownApplication.class, args);
+    }
 
-	@GetMapping("/health")
-	public String health() {
-        String message = String.format("Service is healthy! Current time: %s",
-            Calendar.getInstance().getTime());
-		return message;
-	}
+    @Getter
+    @Setter
+    public class HealthMessage {
+        private String message;
+        private String timestamp;
+
+        public HealthMessage(String message, String timestamp) {
+            this.message = message;
+            this.timestamp = timestamp;
+        }
+    }
+
+    /**
+     * Endpoint for service health check
+     *
+     * @return JSON object with message and timestamp
+     */
+    @ResponseStatus(HttpStatus.OK)
+    @GetMapping("/health")
+    public HealthMessage health() {
+        return new HealthMessage(
+            "Service is healthy!",
+            Calendar.getInstance().getTime().toString()
+        );
+
+    }
 }
